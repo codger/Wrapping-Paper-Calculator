0738d213094b32d86384ee94b42707ec273b942e
John James <jjames@pacbell.net>
2020-12-22 11:54:23 -0800

    Initial Commit

---
+++ Shared/Wrapping_Paper_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wrapping_Paper_CalculatorApp.swift
+//  Shared
+//
+//  Created by John James on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Wrapping_Paper_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
